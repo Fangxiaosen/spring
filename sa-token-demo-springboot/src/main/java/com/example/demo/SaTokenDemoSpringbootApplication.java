1ec6056a0e17243eaee7cca09e0d57bd7200c968
Administrator <943584003@qq.com>
2022-05-18 09:36:36 +0800

    2022.5.18

---
+++ sa-token-demo-springboot/src/main/java/com/example/demo/SaTokenDemoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaTokenDemoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenDemoSpringbootApplication.class, args);
+    }
+
+}
